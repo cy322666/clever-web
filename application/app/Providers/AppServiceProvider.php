d152a1ca11317e1980f3b658bcf5dd2683cb36d4
cy322666 <cy322666@gmail.com>
2023-05-27 22:49:07 +0200

    prod https env

--- application/app/Providers/AppServiceProvider.php
+++ application/app/Providers/AppServiceProvider.php
@@ -5,6 +5,9 @@ namespace App\Providers;
 use Filament\Facades\Filament;
 use Filament\Navigation\NavigationGroup;
 use Filament\Navigation\NavigationItem;
+use Illuminate\Support\Env;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,6 +25,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (App::environment() == 'prod') {
 
+            URL::forceScheme('https');
+        }
     }
 }
