60e1dd31d79fbabbe0767b5ddb4c47dbef5096cf
cy322666 <cy322666@gmail.com>
2023-08-24 14:54:18 +0200

    fix auth

--- application/app/Http/Controllers/Api/AuthController.php
+++ application/app/Http/Controllers/Api/AuthController.php
@@ -40,7 +40,7 @@ class AuthController extends Controller
 
         (new Client($account))->init();
 
-        return route('filament.app.resources.core.users.view', ['record' => $user]);
+        redirect(route('filament.app.resources.core.users.view', ['record' => $user]));
     }
 
     public function secrets(Request $request)
