01d47c2a8fccf98017171047bbc55b9bc0bb2ed4
cy322666 <cy322666@gmail.com>
2023-08-24 13:53:28 +0200

    user fix access

--- application/app/Models/User.php
+++ application/app/Models/User.php
@@ -54,7 +54,7 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        if ($panel->id('admin')) {
+        if ($panel->getId() == 'admin') {
 
             return $this->is_root;
         }
