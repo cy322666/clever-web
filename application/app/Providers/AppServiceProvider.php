27f779521e4aaee791f8b381bbe69b4e4e84dfa0
cy322666 <cy322666@gmail.com>
2023-08-31 15:00:03 +0200

    fix prod

--- application/app/Providers/AppServiceProvider.php
+++ application/app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if($this->app->environment('prod')) {
+        if($this->app->environment('production')) {
 
             URL::forceScheme('https');
         }
