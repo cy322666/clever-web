44b978dd0488a868628aed36d17db5e1ce6460af
cy322666 <cy322666@gmail.com>
2023-09-01 14:43:44 +0200

    amo fields add

---
+++ application/app/Models/amoCRM/Field.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models\amoCRM;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Field extends Model
+{
+    use HasFactory;
+
+    protected $table = 'amocrm_fields';
+
+    protected $fillable = [
+        'field_id',
+        'name',
+        'type',
+        'code',
+        'sort',
+        'is_api_only',
+        'entity_type',
+        'enums',
+    ];
+}
