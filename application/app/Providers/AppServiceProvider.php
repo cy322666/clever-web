52b03d4e12c036a36d677136605eef88a3b9f2dc
cy322666 <cy322666@gmail.com>
2023-09-11 16:17:31 +0200

    add amochat

--- application/app/Providers/AppServiceProvider.php
+++ application/app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         FilamentAsset::register([
-            Js::make('amochat', __DIR__ . '/../../resources/js/custom.js'),
+            Js::make('amochat', __DIR__ . resource_path('/js/amochat.js')),
         ]);
 
         if($this->app->environment('production')) {
