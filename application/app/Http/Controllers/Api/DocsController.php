42953a8d6fadc41821d15161c7454347c1d69240
cy322666 <cy322666@gmail.com>
2023-09-19 21:40:40 +0200

    add route redirect docs

--- application/app/Http/Controllers/Api/DocsController.php
+++ application/app/Http/Controllers/Api/DocsController.php
@@ -10,6 +10,6 @@ class DocsController extends Controller
 {
     public function redirect(Request $request)
     {
-        Log::info(__METHOD__, file_get_contents('php://input'));
+        Log::info(__METHOD__, [file_get_contents('php://input')]);
     }
 }
