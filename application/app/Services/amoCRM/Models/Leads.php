75248bf8522a06ece410b86a9a05732edefd0f6b
cy322666 <cy322666@gmail.com>
2023-09-20 20:16:18 +0200

    add time updated at leads

--- application/app/Services/amoCRM/Models/Leads.php
+++ application/app/Services/amoCRM/Models/Leads.php
@@ -100,7 +100,7 @@ abstract class Leads
             $lead->status_id = $params['status_id'];
 
         $lead->contacts_id = $contact->id;
-        $lead->updated_at = time() + 5;
+        $lead->updated_at = time() + 30;
         $lead->save();
 
         return $lead;
