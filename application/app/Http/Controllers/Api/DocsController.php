414b71ec3b2b7155b3c062e85ad16a95fd8db0f6
cy322666 <cy322666@gmail.com>
2023-09-20 10:43:45 +0200

    fix amo

--- application/app/Http/Controllers/Api/DocsController.php
+++ application/app/Http/Controllers/Api/DocsController.php
@@ -5,11 +5,15 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\URL;
 
 class DocsController extends Controller
 {
     public function redirect(Request $request)
     {
+        dd($_GET);
+
         Log::info(__METHOD__, $request->all());
+        //https://oauth.yandex.ru/authorize?response_type=token&client_id=b5ecb51c6a594755a2faefda59c2c6a9&state=test
     }
 }
