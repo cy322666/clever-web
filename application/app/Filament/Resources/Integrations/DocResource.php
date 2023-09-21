1768e389a314b550dfed2b45bdcdb9ab0e689f8e
cy322666 <cy322666@gmail.com>
2023-09-21 15:35:44 +0200

    add doc

--- application/app/Filament/Resources/Integrations/DocResource.php
+++ application/app/Filament/Resources/Integrations/DocResource.php
@@ -25,6 +25,8 @@ class DocResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
+    protected static ?string $recordTitleAttribute = 'Документы';
+
     public static function form(Form $form): Form
     {
         return $form
