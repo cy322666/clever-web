98e3803ff8f22b8a6096392c7a93df1fe227bed7
cy322666 <cy322666@gmail.com>
2024-05-31 10:15:05 +0300

    fix

--- application/app/Services/Doc/FormatService.php
+++ application/app/Services/Doc/FormatService.php
@@ -56,7 +56,7 @@ abstract class FormatService
                 ->first();
 
             if ($field)
-                return $entities[$field->entity_type]->cf($field->name)->getValue();
+                return $entities[$field->entity_type]->byId($field->field_id)->getValue();
 
         } catch (\Throwable $e) {
 
