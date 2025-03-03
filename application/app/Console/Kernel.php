fc065eab8647b278fcd941156276445b6d2b870b
cy322666 <cy322666@gmail.com>
2025-03-03 17:20:25 +0200

    queue fix

--- application/app/Console/Kernel.php
+++ application/app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
 //        $schedule->command('horizon:snapshot')->everyFiveMinutes();
         $schedule->command('telescope:prune')->daily();
 
-        $schedule->command('app:failed-jobs')->everyMinute();
+//        $schedule->command('app:failed-jobs')->everyMinute();
 //        $schedule->command('backup:run --db-name='.env('DB_CONNECTION').' --only-db')->dailyAt('00:00');
         // $schedule->command('inspire')->hourly();
     }
