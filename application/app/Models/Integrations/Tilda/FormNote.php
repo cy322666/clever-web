9b113cf993285b61628de63220a7e277114b800d
cy322666 <cy322666@gmail.com>
2025-07-26 15:03:10 +0200

    fix tilda

--- application/app/Models/Integrations/Tilda/FormNote.php
+++ application/app/Models/Integrations/Tilda/FormNote.php
@@ -50,7 +50,7 @@ abstract class FormNote
 
                 try {
 
-                    $value .= str_replace(['\u0026quot;', '&quot;'], '"', $product->name)."\n";;
+                    $value .= str_replace(['\u0026quot;', '&quot;'], '"', $product->value)."\n";;
 
                     $note = array_merge($note, ['- '.$key.' : '.$value]);
 
