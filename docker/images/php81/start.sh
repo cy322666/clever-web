e313485e5878d7f2c8580eda0ccf3a22fe6b870f
cy322666 <cy322666@gmail.com>
2025-07-28 18:13:17 +0200

    del php octane

--- docker/images/php81/start.sh
+++ docker/images/php81/start.sh
@@ -7,12 +7,15 @@ composer install
 
 #php artisan key:generate
 
-composer require laravel/octane --with-all-dependencies
+#composer require laravel/octane --with-all-dependencies
 
 php artisan telescope:install
 
-php artisan horizon:install
+#php artisan horizon:install
 
-php artisan octane:install --server="swoole"
+#php artisan octane:install --server="swoole"
 
-php artisan octane:start --server="swoole" --host="0.0.0.0" --workers=10 --task-workers=1 --max-requests=100 --watch;
+#php artisan octane:start --server="swoole" --host="0.0.0.0" --workers=10 --task-workers=1 --max-requests=100 --watch;
+
+php artisan serve
+#--port=8080
