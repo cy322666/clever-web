753413e6143b86556d684cbb24ff5f223548dbe2
cy322666 <cy322666@gmail.com>
2025-10-15 08:52:26 +0200

    fix clever

--- application/resources/views/livewire/clever/bayers/form-order.blade.php
+++ application/resources/views/livewire/clever/bayers/form-order.blade.php
@@ -1,4 +1,13 @@
-<form wire:submit.prevent="create">
-    {{ $this->form }}
-    <x-filament::button type="submit">Сохранить</x-filament::button>
+<form wire:submit.prevent="create" class="max-w-2xl mx-auto">
+    <x-filament::section>
+        <x-slot name="heading">Создание заказа</x-slot>
+
+        {{ $this->form }}
+
+        <x-slot name="footer">
+            <x-filament::button type="submit">
+                Сохранить
+            </x-filament::button>
+        </x-slot>
+    </x-filament::section>
 </form>
