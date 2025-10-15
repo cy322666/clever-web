fc64974315a94a2d8194dbb7b8e354ab34ce22ea
cy322666 <cy322666@gmail.com>
2025-10-15 08:50:53 +0200

    fix clever

--- application/resources/views/livewire/clever/bayers/form-order.blade.php
+++ application/resources/views/livewire/clever/bayers/form-order.blade.php
@@ -1,33 +1,4 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <title>Форма заявки</title>
-
-    {{-- Подключаем стили Filament --}}
-    @filamentStyles
-</head>
-<body class="antialiased bg-gray-50">
-
-<div class="max-w-lg mx-auto mt-10 p-6 bg-white rounded-xl shadow-md">
-    <form wire:submit="submit">
-        {{ $this->form }}
-        <x-filament::button type="submit" class="mt-4">
-            Отправить
-        </x-filament::button>
-    </form>
-</div>
-
-{{-- Подключаем скрипты Filament --}}
-{{--@filamentScripts--}}
-{{--@livewireScripts--}}
-</body>
-</html>
-
-
-{{--<x-filament::layouts.app>--}}
-{{--    <div class="max-w-2xl mx-auto mt-10">--}}
-{{--        {{ $this->form }}--}}
-{{--    </div>--}}
-{{--</x-filament::layouts.app>--}}
-
+<form wire:submit.prevent="create">
+    {{ $this->form }}
+    <x-filament::button type="submit">Сохранить</x-filament::button>
+</form>
