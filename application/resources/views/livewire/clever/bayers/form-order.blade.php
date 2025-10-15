428cefae0121b6548cae69edd929bed4eb9d478d
cy322666 <cy322666@gmail.com>
2025-10-15 09:01:43 +0200

    fix clever

--- application/resources/views/livewire/clever/bayers/form-order.blade.php
+++ application/resources/views/livewire/clever/bayers/form-order.blade.php
@@ -1,19 +1,15 @@
-<form wire:submit.prevent="create" class="max-w-2xl mx-auto">
+<form wire:submit.prevent="create" class="max-w-2xl mx-auto space-y-6">
     <x-filament::card>
         <x-slot name="heading">
             Создание заказа
         </x-slot>
 
         {{ $this->form }}
-
-        @if (View::exists('filament::components.card.footerActions'))
-            <x-slot name="footerActions">
-                <x-filament::button type="submit">Сохранить</x-filament::button>
-            </x-slot>
-        @else
-            <x-slot name="footer">
-                <x-filament::button type="submit">Сохранить</x-filament::button>
-            </x-slot>
-        @endif
     </x-filament::card>
+
+    <div class="text-right">
+        <x-filament::button type="submit">
+            Сохранить
+        </x-filament::button>
+    </div>
 </form>
