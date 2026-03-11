8650a00b8bac9b826a6802934b5cd000250848a4
cy322666 <cy322666@gmail.com>
2026-03-11 11:25:43 +0200

    yc fix

--- application/app/Services/YClients/Notes.php
+++ application/app/Services/YClients/Notes.php
@@ -4,6 +4,7 @@ namespace App\Services\YClients;
 
 use App\Models\Integrations\YClients\Record;
 use App\Models\Integrations\YClients\Setting;
+use App\Services\amoCRM\Client;
 use Illuminate\Http\Client\ConnectionException;
 use Ufee\Amo\Models\Lead as LeadModel;
 
@@ -12,18 +13,21 @@ class Notes
     /**
      * @throws ConnectionException
      */
-    public static function createNoteLead(YClients $client, Record $record, LeadModel $lead): void
+    public static function createNoteLead(YClients $client, Record $record, LeadModel $lead, Client $amoApi): void
     {
-        $note = $lead->createNote();
+        $note = $amoApi->service->notes()->create();
 
+        $note->element_id = $lead->id;
+        $note->element_type = 2;
+        $note->note_type = 4;
         $note->text = implode("\n", [
-            ' - Запись №'.$record->record_id,
-            ' - Событие : '.$record->getEvent(),
-            ' - Филиал : '.$client->getBranchTitle($record->company_id),
-            ' - Процедуры : '.$record->title,
-            ' - Дата и Время : '.$record->datetime,
-            ' - Мастер : '.$record->staff_name,
-            ' Комментарий : '.$record->comment,
+            ' - Запись №' . $record->record_id,
+            ' - Событие : ' . $record->getEvent(),
+            ' - Филиал : ' . $client->getBranchTitle($record->company_id),
+            ' - Процедуры : ' . $record->title,
+            ' - Дата и Время : ' . $record->datetime,
+            ' - Мастер : ' . $record->staff_name,
+            ' Комментарий : ' . $record->comment,
         ]);
         $note->save();
     }
